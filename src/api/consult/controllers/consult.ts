70ee6d7879eec7260618fc0b5ceeb0d547388743
huangkun <3125330366@qq.com>
2025-07-10 12:03:08 +0800

    Initial commit

---
+++ src/api/consult/controllers/consult.ts
@@ -0,0 +1,7 @@
+/**
+ * consult controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::consult.consult');
---
+++ src/api/consult/routes/consult.ts
@@ -0,0 +1,7 @@
+/**
+ * consult router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::consult.consult');
